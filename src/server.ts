754b03594dbfaa7b41efdd9c03dc4ee3730eb26e
Filipe Góis <fythoy@gmail.com>
2025-05-31 20:43:09 -0300

    implementado swagger

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,36 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import { routes } from "./routes/routes";
 import jwtPlugin from "./plugins/jwt";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const app = fastify({ logger: true });
 
 app.register(cors);
+app.register(fastifySwagger, {
+  swagger: {
+    info: {
+      title: "Minha API",
+      description: "Documentação da API com Swagger",
+      version: "1.0.0",
+    },
+
+    schemes: ["http"],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+  },
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/",
+  uiConfig: {
+    docExpansion: "list",
+    deepLinking: false,
+  },
+  //   staticCSP: true,
+  transformSpecificationClone: true,
+});
 app.register(jwtPlugin);
 app.register(routes);
 
-app.listen({ host: "0.0.0.0", port: 3333 });
+app.listen({ port: 3333, host: "0.0.0.0" });
